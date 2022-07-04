5b6a7dc8aff2329e0baad3d4702024e60e517083
jesustobarra <81744974+jesustobarra@users.noreply.github.com>
2022-07-04 11:48:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module("app",[]);
 app.controller("controlador", function($scope){
     var lista = this;
     lista.productos = [
-        {id: 1, nomre: "producto 1", precio: 10.0},
+        {id: 1, nombre: "producto 1", precio: 10.0},
         {id: 2,nombre: "producto 2", precio:20.0}
     ]
     lista.compras=[];
